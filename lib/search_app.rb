89c5ea8eff686e74567d5ef33523701200854270
Jaya Wijono <jwijono@seek.com.au>
2021-05-30 16:13:12 +1000

    Add SearchApp#init

---
+++ lib/search_app.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'services/generate_database'
+require 'repository'
+require 'json'
+require 'dry/monads'
+require 'dry/monads/do'
+
+class SearchApp
+  class << self
+    include Dry::Monads::Do.for(:init)
+    include Dry::Monads[:try, :result]
+
+    def init(user_json:, organization_json:, ticket_json:)
+      parsed_user_json = yield parse_json(user_json)
+      parsed_organization_json = yield parse_json(organization_json)
+      parsed_ticket_json = yield parse_json(ticket_json)
+
+      db = yield Services::GenerateDatabase.call(
+        'users' => parsed_user_json,
+        'organizations' => parsed_organization_json,
+        'tickets' => parsed_ticket_json
+      )
+
+      Success(new(Repository.new(db)))
+    end
+
+    private
+
+    def parse_json(json)
+      Try[JSON::ParserError] { JSON.parse(json) }.to_result
+    end
+  end
+
+  attr_reader :repo
+
+  def initialize(repo)
+    @repo = repo
+  end
+end
